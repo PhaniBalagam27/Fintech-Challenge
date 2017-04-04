fa064653ab8353a934c06b1f8fbb8c8e05b318de
Phani Teja Balagam <pxb161930@utdallas.edu>
2017-04-04 05:41:46 -0500

    Second

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,6 +6,7 @@ Created on Apr 1, 2017
 
 import pandas as pd
 from nltk.tokenize import word_tokenize
+import nltk
 
 dataframe = pd.read_pickle("C:/Users/phani/Downloads/fintech_cleaned.pkl")
 # print(dataframe.head())
@@ -43,7 +44,7 @@ for t in tok:
 #print(gh)
 
 tokenized_docs = [word_tokenize(doc) for doc in gh]
-print(tokenized_docs)
+#print(tokenized_docs)
 
 from nltk.corpus import stopwords
 
@@ -55,5 +56,41 @@ for doc in tokenized_docs:
             new_term_vector.append(word)
     tokenized_docs_no_stopwords.append(new_term_vector)
 
-print(tokenized_docs_no_stopwords)
+#print(tokenized_docs_no_stopwords)
+'''
+from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
+
+def get_ngrams(text, n ):
+    n_grams = ngrams(word_tokenize(text), n)
+    return [ ' '.join(grams) for grams in n_grams]
+
+bi = get_ngrams(tokenized_docs_no_stopwords, 2)
+tri = get_ngrams(tokenized_docs_no_stopwords, 3)
+print(bi)'''
+
+import itertools
+merged = list(itertools.chain.from_iterable(tokenized_docs_no_stopwords))
+
+all_words = []
+for w in merged:
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+print(all_words.most_common(15))
+
+word_features = list(all_words.keys())[:500]
+
+def find_features(tw):
+    words = set(tw)
+    feature = {}
+    for w in word_features:
+        feature[w]=(w in words)
+    return feature
+print((find_features(all_words)))
+
+featuresets = [(find_features(rev), category) for rev,category in word_features]
+
+
+
 
